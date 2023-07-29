2d175497a2c7ab463677f5aa5830704fa5242c3b
zhihuacc <zhihua.che@gmail.com>
2023-07-29 17:24:52 +0800

    1) add docker/build_dev_img.sh. 2) comment on glog

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir build
+# if build brpc with glog, then to build 'examples', need add 'glog' in 'set(DYNAMIC_LIB ...)' in CMakeLists.txt
 cmake -B build -DWITH_GLOG=ON
 cmake --build build  -j6
---
+++ docker/build_dev_img.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Build image
+docker build -f Dev.Dockerfile -t brpc_dev ..
